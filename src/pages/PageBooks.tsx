cfbb7177e1cec45c2326bf7b995b73ce14e23566
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-27 14:25:29 +0100

    spinner in

--- src/pages/PageBooks.tsx
+++ src/pages/PageBooks.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
+import { ImSpinner9 } from "react-icons/im";
 
 export const PageBooks = () => {
 	const { userName, books, handleAddBookToCart, cart } =
@@ -19,31 +20,43 @@ export const PageBooks = () => {
 				There are <span className="highlighted">{books.length}</span>{" "}
 				books.
 			</p>
-			<div className="flex gap-3 flex-wrap">
-				{books.map((book) => {
-					return (
-						<div
-							className="mt-2 border-2 border-slate-950 p-2 rounded-lg"
-							key={book.id}
-						>
-							<img
-								className="my-2 w-40 h-fit cursor-pointer"
-								onClick={() => handleAddBookToCart(book)}
-								key={book.id}
-								src={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
-							/>
-							<p className="text-center bg-black text-white p-2 rounded-lg">
-								Ordered:{" "}
-								{
-									cart.items.filter(
-										(item) => item.idCode === book.idCode
-									).length
-								}
-							</p>
-						</div>
-					);
-				})}
-			</div>
+
+			{books.length > 0 ? (
+				<>
+					<div className="flex gap-3 flex-wrap">
+						{books.map((book) => {
+							return (
+								<div
+									className="mt-2 border-2 border-slate-950 p-2 rounded-lg"
+									key={book.id}
+								>
+									<img
+										className="my-2 w-40 h-fit cursor-pointer"
+										onClick={() =>
+											handleAddBookToCart(book)
+										}
+										key={book.id}
+										src={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
+									/>
+									<p className="text-center bg-black text-white p-2 rounded-lg">
+										Ordered:
+										{
+											cart.items.filter(
+												(item) =>
+													item.idCode === book.idCode
+											).length
+										}
+									</p>
+								</div>
+							);
+						})}
+					</div>
+				</>
+			) : (
+				<div className="flex justify-center items-center">
+					<ImSpinner9 className="animate-spin text-7xl	text-orange-500 " />
+				</div>
+			)}
 		</>
 	);
 };
