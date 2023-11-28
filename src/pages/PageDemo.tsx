89b7ad1e00b531e75a19802e8fddc2ce8e15372f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-28 11:27:44 +0100

    count buttons display works

--- src/pages/PageDemo.tsx
+++ src/pages/PageDemo.tsx
@@ -1,7 +1,45 @@
+import { useReducer } from "react";
+
+interface IState {
+	count: number;
+	error: string;
+}
+
+const initialState: IState = {
+	count: 0,
+	error: "",
+};
+
+interface IAction {
+	type: "increment" | "decrement";
+	payload: number;
+}
+
+const reducer = (state: IState, action: IAction) => {
+	const _state = structuredClone(state);
+	switch (action.type) {
+		case "increment":
+			_state.count += action.payload;
+			break;
+		case "decrement":
+			_state.count -= action.payload;
+			break;
+		default:
+			_state.error = "bad action type: " + action.type;
+	}
+	return _state;
+};
 export const PageDemo = () => {
+	const [state] = useReducer(reducer, initialState);
+
 	return (
 		<>
-			<p>this is the demo page</p>
+			<h2 className="text-2xl">Demo of useReducer</h2>
+			<div className="flex gap-3">
+				<button>-</button>
+				<button>+</button>
+				<p className="text-xl mt-1">count = {state.count}</p>
+			</div>
 		</>
 	);
 };
