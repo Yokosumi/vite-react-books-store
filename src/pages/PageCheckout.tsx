39e07020351441d0735623643895fc2001df62d8
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-28 09:42:07 +0100

    only one Checkoutitem component

--- src/pages/PageCheckout.tsx
+++ src/pages/PageCheckout.tsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
 import { CheckOutItem } from "../components/CheckOutItem";
-import { CheckoutItems } from "../components/CheckOutItems";
 
 export const PageCheckout = () => {
 	const { userName, cart } = useContext(AppContext);
@@ -18,19 +17,11 @@ export const PageCheckout = () => {
 				{cart.items.map((book) => {
 					return (
 						<div className="flex gap-3 items-center mb-3">
-							{cart.items.length < 2 ? (
-								<CheckOutItem
-									key={book.id}
-									url={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
-									bookTitle={book.title}
-								/>
-							) : (
-								<CheckoutItems
-									key={book.id}
-									url={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
-									bookTitle={book.title}
-								/>
-							)}
+							<CheckOutItem
+								key={book.id}
+								url={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
+								bookTitle={book.title}
+							/>
 						</div>
 					);
 				})}
