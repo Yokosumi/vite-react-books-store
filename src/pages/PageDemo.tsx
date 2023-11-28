8f784c1ff7ec9c89eadb3a276bc4b4bb918c2c61
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-28 11:51:04 +0100

    added works

--- src/pages/PageDemo.tsx
+++ src/pages/PageDemo.tsx
@@ -2,43 +2,81 @@ import { useReducer } from "react";
 
 interface IState {
 	count: number;
+	title: string;
+	history: string[];
 	error: string;
 }
 
 const initialState: IState = {
 	count: 0,
+	title: "The useReducer/reducer Demo",
+	history: [],
 	error: "",
 };
 
-interface IAction {
+interface INumberAction {
 	type: "increment" | "decrement";
 	payload: number;
 }
+interface IStringAction {
+	type: "setTitle";
+	payload: string;
+}
 
-const reducer = (state: IState, action: IAction) => {
+const reducer = (state: IState, action: INumberAction | IStringAction) => {
 	const _state = structuredClone(state);
 	switch (action.type) {
 		case "increment":
 			_state.count += action.payload;
+			_state.history.push(`added ${action.payload}`);
 			break;
 		case "decrement":
 			_state.count -= action.payload;
 			break;
+		case "setTitle":
+			_state.title = action.payload;
+			break;
 		default:
+			// todo: action.type to string
 			_state.error = "bad action type: " + action.type;
 	}
 	return _state;
 };
 export const PageDemo = () => {
-	const [state] = useReducer(reducer, initialState);
+	const [state, dispatch] = useReducer(reducer, initialState);
 
 	return (
 		<>
-			<h2 className="text-2xl">Demo of useReducer</h2>
+			<h2 className="text-2xl mb-3">{state.title}</h2>
 			<div className="flex gap-3">
-				<button>-</button>
-				<button>+</button>
+				<button
+					onClick={() => dispatch({ type: "decrement", payload: 1 })}
+				>
+					-
+				</button>
+				<button
+					onClick={() => dispatch({ type: "increment", payload: 1 })}
+				>
+					+
+				</button>
 				<p className="text-xl mt-1">count = {state.count}</p>
+			</div>
+			<div className="mb-3">
+				Change title:{" "}
+				<input
+					className="w-80"
+					value={state.title}
+					onChange={(e) =>
+						dispatch({ type: "setTitle", payload: e.target.value })
+					}
+					type="text"
+				/>
+			</div>
+			<div>
+				<h2 className="text-xl2 mb-3">History</h2>
+				{state.history.map((historyItem, index) => {
+					return <p key={index}>{historyItem}</p>;
+				})}
 			</div>
 		</>
 	);
